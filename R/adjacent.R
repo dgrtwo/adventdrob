4e634731bbf71712eda6ad8ad5a41dfef5ee9af1
David Robinson <davidrobinson@davids-mbp.home>
2021-12-11 21:29:52 -0500

    Added a function adjacent_nest

--- R/adjacent.R
+++ R/adjacent.R
@@ -23,3 +23,31 @@ adjacent_join <- function(x, y = x, diagonal = FALSE, suffix = c("", "2")) {
     filter(row != row2 | col != col2) %>%
     select(-row_delta, -col_delta)
 }
+
+#' Nest a column with adjacent values
+#'
+#' @param x A table with columns for row and col
+#' @param y A second table with columns row and col. By default, does
+#' a self-join
+#' @param diagonal Whether diagonal cells are counted as adjacent
+#'
+#' @examples
+#'
+#' library(purrr)
+#' library(dplyr)
+#' grid11 <- advent_input(11) %>%
+#'   grid_tidy(x)
+#'
+#' # One "flash"
+#' grid11 %>%
+#'   mutate(value = value + 1) %>%
+#'   adjacent_nest(diagonal = TRUE) %>%
+#'   mutate(value = value + map_dbl(adjacent_value, ~ sum(. > 9)))
+#'
+adjacent_nest <- function(x, y = x, diagonal = FALSE) {
+  x %>%
+    adjacent_join(y, diagonal = diagonal) %>%
+    select(!!!syms(colnames(x)), value2) %>%
+    tidyr::nest(adjacent_value = value2) %>%
+    mutate(adjacent_value = purrr::map(adjacent_value, 1))
+}
